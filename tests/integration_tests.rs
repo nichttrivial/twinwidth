5722770cf2c29f1989a3b91ecc6a6a7c655e4204
Uwe Warschun <uwewarschun@googlemail.com>
2024-02-10 21:54:09 +0100

    add verifier via .tww file to test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs, path::PathBuf};
+use std::{collections::HashMap, fs, path::PathBuf, process::Command};
 
 use twinwidth::{algo::Greedy, graph::Graph};
 
@@ -35,11 +35,49 @@ pub fn test_tiny_set() {
             .get(path.path().file_name().unwrap().to_str().unwrap())
             .unwrap();
 
+        //check against pre defined expected results
         assert_eq!(
             tw,
             *solution,
             "Graph: {}",
             path.path().file_name().unwrap().to_str().unwrap()
         );
+
+        //check against the verfier provided by the pace challenge
+        write_tww_to_fs(&greedy.output_tww_str());
+        let verifier_result =
+            execute_python_verifier(path.path().file_name().unwrap().to_str().unwrap());
+
+        assert_eq!(tw, verifier_result);
     }
+}
+
+//This function expects that the right result is placed in result.tww before calling.
+fn execute_python_verifier(filename: &str) -> usize {
+    let mut script = env!("CARGO_MANIFEST_DIR").to_string();
+    script.push_str("/pacechallenge/verifier.py");
+
+    let mut graph = env!("CARGO_MANIFEST_DIR").to_string();
+    graph.push_str("/pacechallenge/tiny-set/");
+    graph.push_str(filename);
+
+    let mut result = env!("CARGO_MANIFEST_DIR").to_string();
+    result.push_str("/pacechallenge/result.tww");
+
+    let output = Command::new("python")
+        .args([script, graph, result])
+        .output()
+        .unwrap();
+
+    let width = String::from_utf8(output.stdout).unwrap();
+    let width = width.split(":").collect::<Vec<&str>>()[1].trim();
+    let width: usize = width.parse().expect("Unable to parse number");
+    width
+}
+
+fn write_tww_to_fs(tww: &str) {
+    let mut path = env!("CARGO_MANIFEST_DIR").to_string();
+    path.push_str("/pacechallenge/result.tww");
+
+    fs::write(path, tww).unwrap();
 }
